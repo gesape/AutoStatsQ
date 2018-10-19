1bea874ae8ff7a0f3f0b37f2857c0486d292ac74
Gesa <gesap@gfz-potsdam.de>
2018-10-19 14:37:51 +0200

    upd setup

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ setup(
     version=version,
     author='Gesa Petersen',
     author_email='gesap@gfz-potsdam.de',
-    license='GPLv3',
     python_requires='!=3.0.*, !=3.1.*, !=3.2.*, <4',
     install_requires=[],
     packages=['autostatsq'],
