bd1ab940ea79bdd9f3065aa33801b76741c8176d
gesap <gesap@gfz.de>
2025-10-08 17:37:04 +0200

    flake8 make_report

--- src/make_report.py
+++ src/make_report.py
@@ -15,28 +15,37 @@ logger = logs
 
 file_path = os.path.abspath(os.path.dirname(__file__))
 
+
 def gen_report(config):
     logger.info('yes')
     tpl_file = os.path.join(file_path, 'generate_report/index_template.html')
 
-    configfile = config  # '../../../../AutoStatsQ_settings.config'
+    configfile = config
 
-    gensettings, catalogconf, arrTconf, metaDataconf, RestDownconf,\
+    gensettings, catalogconf, arrTconf, metaDataconf, RestDownconf, \
             synthsconf, gainconf, psdsconf, orientconf, timingconf, tc, maps =\
             AutoStatsQConfig.load(filename=configfile).Settings
 
     try:
-        os.makedirs(os.path.join(gensettings.work_dir, 'result_report'))
+        os.makedirs(os.path.join(gensettings.work_dir,
+                                 'result_report'))
     except:
-        rmtree(os.path.join(gensettings.work_dir, 'result_report'))
-        os.makedirs(os.path.join(gensettings.work_dir, 'result_report'))
+        rmtree(os.path.join(gensettings.work_dir,
+                            'result_report'))
+        os.makedirs(os.path.join(gensettings.work_dir,
+                                 'result_report'))
 
     html_directories = ['figures', 'reveal', 'theme']
     for h in html_directories:
-        copytree(src=os.path.join(file_path, 'generate_report', h),
-                        dst=os.path.join(os.path.join(gensettings.work_dir, 'result_report'), h))
+        copytree(src=os.path.join(file_path,
+                                  'generate_report', h),
+                 dst=os.path.join(os.path.join(
+                                  gensettings.work_dir,
+                                  'result_report'), h))
 
-    outfile = os.path.join(gensettings.work_dir, 'result_report', 'index_report.html')
+    outfile = os.path.join(gensettings.work_dir,
+                           'result_report',
+                           'index_report.html')
 
     result_dir = os.path.join(gensettings.work_dir, 'results')
 
@@ -52,52 +61,66 @@ def gen_report(config):
             if "INCLUDE_HERE_CAT" in line:
                 logger.info('Including catalog in html report.')
                 cat_result_dir = os.path.join(result_dir, 'catalog')
-                extended_lines.append(cat.include_maps_subsets(tmin, tmax, cat_result_dir))
-                extended_lines.append(cat.include_event_tabels(tmin, tmax, Mmin, cat_result_dir))
-                extended_lines.append(cat.include_statistic_plots(cat_result_dir))
+                extended_lines.append(cat.include_maps_subsets(
+                                      tmin, tmax, cat_result_dir))
+                extended_lines.append(cat.include_event_tabels(
+                                      tmin, tmax, Mmin, cat_result_dir))
+                extended_lines.append(cat.include_statistic_plots(
+                                      cat_result_dir))
 
             elif "INCLUDE_HERE_ORIENT" in line:
                 logger.info('Including orientation test results in html report.')
                 orient_result_dir = os.path.join(result_dir, 'orient')
                 extended_lines.append(ori.include_map(orient_result_dir,
                     orientconf.ccmin))
-                extended_lines.append(ori.include_table_correction_angles(orient_result_dir,
-                    orientconf.ccmin))
-                extended_lines.append(ori.add_single_station_results(orient_result_dir,
-                    orientconf.ccmin))
-                extended_lines.append(ori.add_all_stats_figures_baz(orient_result_dir,
-                    orientconf.ccmin))
-                extended_lines.append(ori.add_all_stats_figures_timing(orient_result_dir,
-                    orientconf.ccmin))
-                extended_lines.append(ori.add_all_stats_figures_ccs(orient_result_dir,
-                    orientconf.ccmin))
-            
+                extended_lines.append(ori.include_table_correction_angles(
+                    orient_result_dir, orientconf.ccmin))
+                extended_lines.append(ori.add_single_station_results(
+                    orient_result_dir, orientconf.ccmin))
+                extended_lines.append(ori.add_all_stats_figures_baz(
+                    orient_result_dir, orientconf.ccmin))
+                extended_lines.append(ori.add_all_stats_figures_timing(
+                    orient_result_dir, orientconf.ccmin))
+                extended_lines.append(ori.add_all_stats_figures_ccs(
+                    orient_result_dir, orientconf.ccmin))
+
             elif "INCLUDE_HERE_GAINS" in line:
                 logger.info('Including gain test results in html report.')
                 gain_result_dir = os.path.join(result_dir, 'gains')
                 
                 for comp in ['Z', 'R', 'T']:
-                    extended_lines.append(gains.add_gain_map(comp, gain_result_dir))                    
-                    extended_lines.append(gains.add_gain_median_mean(comp, gainconf.gain_factor_method, gain_result_dir))
-                    extended_lines.append(gains.add_gain_allevents_plot(comp, gain_result_dir))
-                    extended_lines.append(gains.add_single_station_results(comp, gain_result_dir))
-            
+                    extended_lines.append(gains.add_gain_map(
+                                          comp, gain_result_dir))
+                    extended_lines.append(gains.add_gain_median_mean(
+                                          comp, gainconf.gain_factor_method,
+                                          gain_result_dir))
+                    extended_lines.append(gains.add_gain_allevents_plot(
+                                          comp, gain_result_dir))
+                    extended_lines.append(gains.add_single_station_results(
+                                          comp, gain_result_dir))
+
             elif "INCLUDE_HERE_PSDS" in line:
                 logger.info('Including PSD test results in html report.')
                 psd_result_dir = os.path.join(result_dir, 'freq')
-                
-                for comp in ['Z', 'R', 'T']:
-                    extended_lines.append(psds.add_psds_stats(comp, psd_result_dir))
-                    extended_lines.append(psds.add_psds_flatranges(comp, psd_result_dir))
 
-                extended_lines.append(psds.include_table_psds_flatranges(psd_result_dir))
+                for comp in ['Z', 'R', 'T']:
+                    extended_lines.append(psds.add_psds_stats(
+                                          comp, psd_result_dir))
+                    extended_lines.append(psds.add_psds_flatranges(
+                                          comp, psd_result_dir))
+
+                extended_lines.append(psds.include_table_psds_flatranges(
+                                      psd_result_dir))
 
             elif "INCLUDE_HERE_TIMING" in line:
                 logger.info('Including timing test results in html report.')
                 time_result_dir = os.path.join(result_dir, 'timing')
-                extended_lines.append(time.add_timing_results(time_result_dir))
-                extended_lines.append(time.add_timing_array(time_result_dir))
-                extended_lines.append(time.add_timing_perstation(time_result_dir))
+                extended_lines.append(time.add_timing_results(
+                                      time_result_dir))
+                extended_lines.append(time.add_timing_array(
+                                      time_result_dir))
+                extended_lines.append(time.add_timing_perstation(
+                                      time_result_dir))
 
             else:
                 extended_lines.append(line)
